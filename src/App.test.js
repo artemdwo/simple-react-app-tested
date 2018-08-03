0df30dda5fa7e9fa5c79ace4f13b7065adfd4bb1
ArtemDwo <artemdwo@gmail.com>
2018-08-03 09:12:30 +0100

    [UPDATE] App.js test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({adapter: new Adapter()});
+
+describe("<App />", () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    // expect(wrapper.find(App)).toHaveReturned;
+    expect(wrapper.find(App)).toHaveReturned;
+  });
 });
