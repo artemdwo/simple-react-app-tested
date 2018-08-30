a5fc74c7785db9ea1eb8281a591a6c38c3ae482d
Artem.Dvornichenko <artemdwo@gmail.com>
2018-08-30 12:09:58 +0100

    [UPDATE] TestCafe tests
    - minor styling changes
    - .only filter has been removed from 'The main <div> has 3 <div> children styled with Person class'
    - 'The main <div> has 3 <div> children styled with Person class' to have className assertion in addition to the count

--- testcafe/tc_first.test.js
+++ testcafe/tc_first.test.js
@@ -2,27 +2,27 @@ import { Selector } from 'testcafe'
 
 fixture `homePage` .page `http://localhost:3001`
 
-test ('The app has main <div> styled as App class', async t => {
+test('The app has main <div> styled as App class', async t => {
   await t
     .expect(Selector('#root>div').withAttribute('class', 'App'))
 })
 
-test ('The main <div> should not have any <Person> children div\'s by default', async t => {
+test('The main <div> should not have any <Person> children div\'s by default', async t => {
   await t
     .expect(Selector('#root>div').child('.Person').count).eql(0)
 })
 
-test ('"Toggle Person list" button is present', async t => {
+test('"Toggle Person list" button is present', async t => {
   await t
     .expect(Selector('#personToggler').exists).ok()
     .expect(Selector('#personToggler').textContent).contains('Toggle Persons list')
 })
 
-const AppDiv = Selector('#root>.App>div')
+const App2DivDown = Selector('.App>div div')
 
-test.only ('The main <div> has 3 <div> children styled with Person class', async t => {
+test('The main <div> has 3 <div> children styled with Person class', async t => {
   await t
     .click('#personToggler')
-    .expect(AppDiv.child('div').count).eql(3)
+    .expect(App2DivDown.hasClass('Person')).ok()
+    .expect(App2DivDown.count).eql(3)
 })
-
